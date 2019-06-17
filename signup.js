f19f9e73ab25dca51f3083d632581795b961afb1
madeInAba <ezeokecc@gmail.com>
2019-06-17 06:03:16 +0100

    I am almost done with this project. I am only having challenges removing deleted tasks from the local storage. This is my fastest CRUD project till date. It took me less than 2 hours to get this running

---
+++ signup.js
@@ -0,0 +1,41 @@
+const fName = document.querySelector('#fName');
+const lName = document.querySelector('#lName');
+const uName = document.querySelector('#uName');
+const mail = document.querySelector('#mail');
+const pWord = document.querySelector('#pWord');
+const sex = document.querySelector('#sex');
+const age = document.querySelector('#age');
+
+const sForm = document.querySelector('#sForm');
+
+sForm.addEventListener('submit', signUp);
+
+function signUp(e) {
+  let pDetails = {
+    'first Name': fName.value,
+    'last Name': lName.value,
+    'user Name': uName.value,
+    email: mail.value,
+    password: pWord.value,
+    sex: sex.value,
+    age: age.value
+  }
+
+  let detailsArr = [];
+  if (fName.value == '' || lName.value == '' || uName.value == '' || mail.value == '' || pWord.value == '' || sex.value == '' || age.value == '') {
+    alert('Please fill in all the details')
+  } else {
+    if (localStorage.getItem('details') == null) {
+      detailsArr.push(pDetails);
+      localStorage.setItem('details', JSON.stringify(detailsArr))
+    } else {
+      detailsArr = JSON.parse(localStorage.getItem('details'));
+      detailsArr.push(pDetails);
+      localStorage.setItem('details', JSON.stringify(detailsArr))
+    }
+
+    location.replace('login.html');
+    e.preventDefault()
+  }
+
+}
