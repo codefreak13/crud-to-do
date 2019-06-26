3b2a0eb07efba584b10e8b3697010a2299f7ad89
madeInAba <ezeokecc@gmail.com>
2019-06-26 20:59:51 +0100

    edited

--- login.js
+++ login.js
@@ -1,72 +1,53 @@
 const logUser = document.querySelector('#logUser');
 const logPass = document.querySelector('#logPass');
 const lForm = document.querySelector('#lf');
-// const avatarName = document.querySelector('#avatarName');
 
-// avatar = filterArr[0]['first Name'];
-// const signUp = document.querySelector('#signup')
-// const pFirstName = document.getElementById('pFirstName');
-// let avatar;
 lForm.addEventListener('click', login);
-// signUp.addEventListener('click', goSign);
-// let filterArr;
+
 function loginCheck(a, b) {
+
   let savedData = JSON.parse(localStorage.getItem('details'));
-// console.log(savedData.password, 'hdgxd')
-  let filterArr = savedData.filter(function(item){
+
+  let filterArr = savedData.filter(function (item) {
     return a === item['user Name'] && b === item.password
   })
-  // savedData.map(data => console.log(data['first Name'], 'paaaaa'))                                                           
-  // console.log(filterArr, 'hhhh')
 
-  // let firstName = filterArr.map(i => {
-  //   i.password
-  // })
+  if (filterArr.length > 0) {
+    //  let avatar = filterArr[0]['first Name'];
+    alert('Login Successful')
+   return location.replace('crud.html')
+  } else if(logUser.value === '' || logPass.value === '')
+    { alert('Please put in your details')
+}else{
+    alert('Username or Password incorrect!')
 
-  // console.log(firstName, 'kjdc')
+    // const matchArr = savedData.find(function (detail) {
+    //   return a === detail['user Name'] && b === detail.password
+    // })
 
-  if (filterArr.length>0) {
-  avatar = filterArr[0]['first Name'];
-  alert('Login Successful', avatar)
-     // console.log(firstName)
-      // pFirstName.textContent = firstName["first Name"]
-       location.replace('crud.html')
-    } else {
-      alert('Username or Password incorrect!')
+    // const firstName = matchArr.map(function (detail) {
+    //   // if(matchArr){
+    //    return detail
 
-  // const matchArr = savedData.find(function (detail) {
-  //   return a === detail['user Name'] && b === detail.password
-  // })
+    // })
+    // console.log(firstName['first Name'])
 
-  // const firstName = matchArr.map(function (detail) {
-  //   // if(matchArr){
-  //    return detail
-    
-  // })
-  // console.log(firstName['first Name'])
-  
-  // if (matchArr) {
-  //   // console.log(firstName)
-  //   // pFirstName.textContent = firstName["first Name"]
-  //   //  location.replace('crud.html')
-  // } else {
-  //   alert('put in the right values')
+    // if (matchArr) {
+    //   // console.log(firstName)
+    //   // pFirstName.textContent = firstName["first Name"]
+    //   //  location.replace('crud.html')
+    // } else {
+    //   alert('put in the right values')
   }
-  // console.log(name)
-  console.log(avatar)
+
 }
-// console.log(avatar)
 
 function login() {
   let user = logUser.value;
   let pass = logPass.value;
- loginCheck(user, pass);
- console.log(avatar)
-//  getAvatar(avatar)
-  // return avatar;
+  loginCheck(user, pass);
 }
 
 function goSign() {
   location.replace('signup.html')
 }
-
