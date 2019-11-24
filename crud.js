cff0cab83f60358df27ea587b12e2a3d3d70c8db
Ezeoke Chukwubuikem <ezeokecc@gmail.com>
2019-11-24 12:45:35 +0100

    resolved my transfer protocl bug

--- crud.js
+++ crud.js
@@ -1,110 +1,107 @@
-const taskInput = document.querySelector('#taskInput');
-const taskList = document.querySelector('#taskList');
-const taskBtn = document.querySelector('#taskBtn');
+const taskInput = document.querySelector("#taskInput");
+const taskList = document.querySelector("#taskList");
+const taskBtn = document.querySelector("#taskBtn");
 
-taskBtn.addEventListener('click', fire);
-document.addEventListener('DOMContentLoaded', persistor);
-taskList.addEventListener('click', removeTask)
+taskBtn.addEventListener("click", fire);
+document.addEventListener("DOMContentLoaded", persistor);
+taskList.addEventListener("click", removeTask);
 
 function fire() {
-
-  addTask()
+  addTask();
   saveTask();
 
-  taskInput.value = '';
+  taskInput.value = "";
 }
 
 function addTask() {
-  if (taskInput.value === '') {
-    alert('please input a task');
+  if (taskInput.value === "") {
+    alert("please input a task");
   } else {
-    taskList.style.display = 'block';
-    let li = document.createElement('li');
-    li.className = 'pTask';
+    taskList.style.display = "block";
+    let li = document.createElement("li");
+    li.className = "pTask";
     li.appendChild(document.createTextNode(taskInput.value));
-    let a = document.createElement('a');
-    let img = document.createElement('img');
-    img.setAttribute('src', 'images/cancel.svg');
-    img.className = 'delete';
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    img.setAttribute("src", "images/cancel.svg");
+    img.className = "delete";
     a.appendChild(img);
     li.appendChild(a);
-    taskList.appendChild(li)
+    taskList.appendChild(li);
   }
 }
 
 function saveTask() {
   let taskArr = [];
-  if (taskInput.value !== '') {
-    if (localStorage.getItem('task') == null) {
+  if (taskInput.value !== "") {
+    if (localStorage.getItem("task") == null) {
       taskArr.push(taskInput.value);
-      localStorage.setItem('task', JSON.stringify(taskArr))
+      localStorage.setItem("task", JSON.stringify(taskArr));
     } else {
-      taskArr = JSON.parse(localStorage.getItem('task'));
+      taskArr = JSON.parse(localStorage.getItem("task"));
       taskArr.push(taskInput.value);
-      localStorage.setItem('task', JSON.stringify(taskArr))
+      localStorage.setItem("task", JSON.stringify(taskArr));
     }
   }
 }
 
 function persistor() {
   let savedData;
-  if (localStorage.getItem('task') == null) {
-    savedData = []
+  if (localStorage.getItem("task") == null) {
+    savedData = [];
   } else {
-    savedData = JSON.parse(localStorage.getItem('task'));
+    savedData = JSON.parse(localStorage.getItem("task"));
   }
 
   savedData.forEach(element => {
-    taskList.style.display = 'block';
-    let li = document.createElement('li');
-    li.className = 'pTask';
+    taskList.style.display = "block";
+    let li = document.createElement("li");
+    li.className = "pTask";
     li.appendChild(document.createTextNode(element));
-    let a = document.createElement('a');
-    let img = document.createElement('img');
-    img.setAttribute('src', 'images/cancel.svg');
-    img.className = 'delete';
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    img.setAttribute("src", "images/cancel.svg");
+    img.className = "delete";
     a.appendChild(img);
     li.appendChild(a);
-    taskList.appendChild(li)
+    taskList.appendChild(li);
   });
 }
 
 function removeTask(e) {
-  if (e.target.classList.contains('delete')) {
+  if (e.target.classList.contains("delete")) {
     e.target.parentElement.parentElement.remove();
-    removeTaskFromLS(e.target.parentElement.parentElement)
-  }; 
+    removeTaskFromLS(e.target.parentElement.parentElement);
+  }
 }
 
 function removeTaskFromLS(value) {
-  const savedData = JSON.parse(localStorage.getItem('task'));
-  savedData.forEach(function (element, index) {
+  const savedData = JSON.parse(localStorage.getItem("task"));
+  savedData.forEach(function(element, index) {
     if (element === value.textContent) {
-      savedData.splice(index, 1)
+      savedData.splice(index, 1);
     }
   });
-  localStorage.setItem('task', JSON.stringify(savedData))
+  localStorage.setItem("task", JSON.stringify(savedData));
 }
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', clearTask);
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", clearTask);
 
 function clearTask() {
-  taskList.style.display = 'none';
-  taskList.innerHTML = '';
-  localStorage.removeItem('task')
+  taskList.style.display = "none";
+  taskList.innerHTML = "";
+  localStorage.removeItem("task");
 }
 
-const signOut = document.querySelector('#signOut');
-signOut.addEventListener('click', out);
+const signOut = document.querySelector("#signOut");
+signOut.addEventListener("click", out);
 
 function out() {
-  location.replace('login.html')
+  location.replace("login.html");
 }
 
-const avatarNameNode = document.querySelector('#avatarName');
- (function() { const [info] = JSON.parse(localStorage.getItem('details'));
-  avatarNameNode.textContent = `Hello, ${info['first Name']}`; })();
-
-
-
+const avatarNameNode = document.querySelector("#avatarName");
+let moniker = JSON.parse(localStorage.getItem("moniker"));
+console.log(moniker);
+avatarNameNode.textContent = `Hello ${moniker}`;
--- login.js
+++ login.js
@@ -1,33 +1,30 @@
-const logUser = document.querySelector('#logUser');
-const logPass = document.querySelector('#logPass');
-const lForm = document.querySelector('#lf');
+const logUser = document.querySelector("#logUser");
+const logPass = document.querySelector("#logPass");
+const lForm = document.querySelector("#lf");
 
 //This is a variable for the expected avatar name
-let moniker;
 
-//this function filters the localstorage of saved data for a match of the inputted value. 
+//this function filters the localstorage of saved data for a match of the inputted value.
 function loginCheck(a, b) {
+  let savedData = JSON.parse(localStorage.getItem("details"));
 
-  let savedData = JSON.parse(localStorage.getItem('details'));
-
-  let filterArr = savedData.filter(function (item) {
-    return a === item['user Name'] && b === item.password
-  })
+  let filterArr = savedData.filter(function(item) {
+    return a === item["user Name"] && b === item.password;
+  });
 
   if (filterArr.length > 0) {
     //this is the value for the moniker expected in the global scope
-     moniker = filterArr[0]['first Name'];
-    alert('Login Successful');
+    let moniker = filterArr[0]["first Name"];
+    localStorage.setItem("moniker", JSON.stringify(moniker));
     //redirects to the homepage if the inputed value has a match in the local storage
-   return location.replace('crud.html')
-  } else if(logUser.value === '' || logPass.value === '')
-    { alert('Please put in your details')
-}else{
-    alert('Username or Password incorrect!')
+    return location.replace("crud.html");
+  } else if (logUser.value === "" || logPass.value === "") {
+    alert("Please put in your details");
+  } else {
+    alert("Username or Password incorrect!");
   }
 }
-
-//This function gets the values from the inputs and runs the loginCheck() 
+//This function gets the values from the inputs and runs the loginCheck()
 function login() {
   let user = logUser.value;
   let pass = logPass.value;
@@ -35,8 +32,4 @@ function login() {
 }
 
 //This is an event listener for the functions above.
-lForm.addEventListener('click', login);
-
-
-
-
+lForm.addEventListener("click", login);
